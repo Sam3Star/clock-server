2f11088c56baa3633c3a2d46587311957317e307
김가영 <yeong.kmj@gmail.com>
2024-06-22 03:40:46 +0900

    ci: init

---
+++ src/main/java/kr/hs/dgsw/clock_server/ClockServerApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.clock_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/hs/dgsw/clock_server/ClockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.clock_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
