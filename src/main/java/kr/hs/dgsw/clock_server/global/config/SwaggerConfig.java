2cc3041d06b9ce787a09a2b80a44e8e1f0cf7829
김가영 <yeong.kmj@gmail.com>
2024-06-24 18:11:38 +0900

    feat: sweagger

---
+++ src/main/java/kr/hs/dgsw/clock_server/global/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package kr.hs.dgsw.clock_server.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+        OpenAPI openAPI = new OpenAPI().addSecurityItem(new SecurityRequirement().addList("JWT Token"))
+                .components(new Components())
+                .info(apiInfo());
+        openAPI.getComponents().addSecuritySchemes("JWT Token", securityScheme);
+        return openAPI;
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Java.")
+                .description("Java API")
+                .version("0.0.1");
+    }
+
+}
