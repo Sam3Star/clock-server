0c96366c88b6f97ff6f5e1dffd636fcdd7bd58b9
김가영 <yeong.kmj@gmail.com>
2024-06-25 19:46:21 +0900

    chore: return state

--- src/main/java/kr/hs/dgsw/clock_server/domain/routine/presentation/dto/res/RoutineLoadRes.java
+++ src/main/java/kr/hs/dgsw/clock_server/domain/routine/presentation/dto/res/RoutineLoadRes.java
@@ -2,17 +2,20 @@ package kr.hs.dgsw.clock_server.domain.routine.presentation.dto.res;
 
 import kr.hs.dgsw.clock_server.global.common.enums.ColorEnum;
 import kr.hs.dgsw.clock_server.global.common.enums.ImportanceEnum;
+import kr.hs.dgsw.clock_server.global.common.enums.State;
 
 public record RoutineLoadRes(
         Long id,
         String name,
         ImportanceEnum importanceEnum,
-        ColorEnum colorEnum
+        ColorEnum colorEnum,
+        State state
 ) {
     public static RoutineLoadRes of(Long id,
                                     String name,
                                     ImportanceEnum importanceEnum,
-                                    ColorEnum colorEnum){
-        return new RoutineLoadRes(id, name, importanceEnum, colorEnum);
+                                    ColorEnum colorEnum,
+                                    State state){
+        return new RoutineLoadRes(id, name, importanceEnum, colorEnum, state);
     }
 }
